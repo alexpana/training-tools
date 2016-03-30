d86bf61757084d3c6d6124d8d30c2665d2be7663
Alexandru Pana <alexandru.pana@tora.com>
2016-03-30 09:56:05 +0300

    Improved solve script error reporting

--- solve.sh
+++ solve.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-template_dir=~/src/training/training-tools
+# the directory where the templates are
+template_dir="$0"
+if [ -L "$template_dir" ]; then
+  template_dir=`readlink -f "$template_dir"`
+fi
+template_dir=`dirname "$template_dir"`
+
+# the solution name argument
 name=$1
+
+# the solution output directory
 solution_dir=`pwd`
-output_template=$solution_dir/$1.rs
 
-cp -n $template_dir/rust_template.rs $solution_dir/$1.rs
+# the solution output path
+output_file=$solution_dir/$1.rs
 
-echo "Template '$1.rs' copied. Good luck!"
+# check if the name is empty
+if [ -z $name ]; then
+  echo "usage: solve solution_name"
+  echo "  solution_name     The name of the output solution file."
+  exit 1
+fi
+
+# check if a solution file already exists
+if [ -e "$output_file" ]; then
+  echo "Solution already exists, you can resume working!"
+else
+  cp -n $template_dir/rust_template.rs $output_file
+  echo "Solution '$1.rs' created. Happy coding!"
+fi
